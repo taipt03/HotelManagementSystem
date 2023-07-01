20627a4444914c1b41241b43c5c4de9dfc1532de
Nam Trần <nam.td215228@sis.hust.edu.vn>
2023-07-01 13:36:03 +0700

    clean code

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,8 +1,7 @@
 <div class="footer-section">
-						<div class="container">
-							<div class="footer-top">
-								<p>Hotel Booking Management Sytem  </p>
-							</div>
-						
-				</div>
-			</div>
+	<div class="container">
+		<div class="footer-top">
+			<p>Hotel Booking Management Sytem </p>
+		</div>
+	</div>
+</div>
